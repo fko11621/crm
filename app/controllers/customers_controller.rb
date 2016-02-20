5b6b349053f00205ee724a453a54d4d7f73cefda
Daisuke Nashiro <nashirox@gmail.com>
2016-02-20 20:05:13 +0900

    1日目終了時点

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,50 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.page(params[:page])
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      # true だよ
+      redirect_to @customer
+    else
+      # false だよ
+      render :new
+    end
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to customers_url
+  end
+
+  private
+    def customer_params
+      params.require(:customer).permit(
+        :family_name, :given_name, :email
+      )
+    end
+end
